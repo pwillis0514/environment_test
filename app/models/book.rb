d1237e542899eabefe520969f5a937507aad6e19
root <root@localhost>
2022-09-17 18:46:32 +0000

    multi book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,10 @@
 class Book < ApplicationRecord
-    validates :title, presence: true
-    validates :author, presence: true
-    validates :price, presence: true
-    validates :publishedDate, presence: true
+    has_many :users, through: :user_books
+    has_many :user_books
+
+    validates :title, :author, :price, :publishedDate, presence: true
+    #validates :title, presence: true
+    #validates :author, presence: true
+    #validates :price, presence: true
+    #validates :publishedDate, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_164447) do
+ActiveRecord::Schema.define(version: 2022_09_17_180124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,19 @@ ActiveRecord::Schema.define(version: 2022_08_26_164447) do
     t.string "author"
     t.float "price"
     t.date "publishedDate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_books", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
